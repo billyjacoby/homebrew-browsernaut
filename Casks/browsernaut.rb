f585e347fd9ffb6c604b50e9e796b7b7909bce57
Billy Jacoby <billyjacoby@gmail.com>
2023-06-14 21:55:35 -0400

    chore: update version to 0.0.2

--- Casks/browsernaut.rb
+++ Casks/browsernaut.rb
@@ -1,9 +1,9 @@
 cask "browsernaut" do
 
-  version "v0.0.2"
-  sha256
+  version "0.0.2"
+  sha256 "f0ad014d1ecb6f8491509b3f00da0648dad0a32e6d9902bcf46756036ecf39b7"
 
-  url "https://github.com/billyjacoby/browsernaut/releases/download/#{version}/Browsernaut_#{version}_x64.dmg"
+  url "https://github.com/billyjacoby/browsernaut/releases/download/v#{version}/Browsernaut_#{version}_x64.dmg"
   name "Browsernaut"
   desc "Browser picker built with Tauri"
   homepage "https://github.com/billyjacoby/browsernaut"
