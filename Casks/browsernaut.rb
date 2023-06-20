cb52c9ec0945946e153a04faa9f2d6c5b4d72b17
Billy Jacoby <billyjacoby@gmail.com>
2023-06-19 21:38:14 -0400

    chore: update version to "1.0.0"

--- Casks/browsernaut.rb
+++ Casks/browsernaut.rb
@@ -1,7 +1,7 @@
 cask "browsernaut" do
 
-  version "1.0.0"
-  sha256 "8f3804a6ecbc5f000927c0d88c596df4bd083ba3805d6134113f623fd7ebd397"
+  version "1.0.1"
+  sha256 ""
 
   url "https://github.com/billyjacoby/browsernaut/releases/download/v#{version}/Browsernaut_#{version}_x64.dmg"
   name "Browsernaut"
