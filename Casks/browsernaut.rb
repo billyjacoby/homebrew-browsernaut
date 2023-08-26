9def95956f15695121e5ac9f76030de3a83181b9
Billy Jacoby <billyjacoby@gmail.com>
2023-08-25 20:42:02 -0400

    fix: filename update

--- Casks/browsernaut.rb
+++ Casks/browsernaut.rb
@@ -3,7 +3,7 @@ cask "browsernaut" do
   version "1.1.1"
   sha256 "188f7ade0ed859b342b9db3832bb775de2cb04c11625fdc9d8c0ae87096790b4"
 
-  url "https://github.com/billyjacoby/browsernaut/releases/download/v#{version}/Browsernaut_#{version}_x64.dmg"
+  url "https://github.com/billyjacoby/browsernaut/releases/download/v#{version}/Browsernaut_#{version}_universal.dmg"
   name "Browsernaut"
   desc "Browser picker built with Tauri"
   homepage "https://github.com/billyjacoby/browsernaut"
