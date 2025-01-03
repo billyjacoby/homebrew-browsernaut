3a5ac791f8b0d938b4455fdd9ff70bff7f88d4c9
billyjacoby <billyjacoby@gmail.com>
2025-01-03 13:20:45 -0500

    chore: update version to "1.3.2"

--- Casks/browsernaut.rb
+++ Casks/browsernaut.rb
@@ -1,7 +1,7 @@
 cask "browsernaut" do
 
   version "1.3.2"
-  sha256 "b085a08d538406c17e3877f66edb081447517433b68100f949f2eb27b0e9c25c"
+  sha256 ""
 
   url "https://github.com/billyjacoby/browsernaut/releases/download/v#{version}/Browsernaut_#{version}_universal.dmg"
   name "Browsernaut"
